03924c758d4b115a357bee1f566886a4889e2871
kongxiangyue <513001363@qq.com>
2018-08-13 14:47:25 +0800

    [A]第五章,service的代码

---
+++ code/chapter05/BroadcastBestPractice/src/main/java/app0/com/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package app0.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.broadcastbestpractice.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
