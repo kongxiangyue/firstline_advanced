e9118b293f6cabab8462850fa6374b0e6d1a4d61
kongxiangyue <513001363@qq.com>
2018-09-05 16:06:08 +0800

    [M]完成例子

--- code/chapter02/e2_2newActivity/src/main/java/app0/com/myapplication/FirstActivity.java
+++ code/chapter02/e2_2newActivity/src/main/java/app0/com/myapplication/FirstActivity.java
@@ -2,6 +2,8 @@ package app0.com.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends Activity {
 
@@ -9,5 +11,12 @@ public class FirstActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_activity);
+        Button button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
