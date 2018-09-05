25034e225af91482f13498353cd57d7dd6f3085f
kongxiangyue <513001363@qq.com>
2018-09-05 16:02:03 +0800

    [M]可以运行

---
+++ code/chapter02/e2_2newActivity/src/main/java/app0/com/myapplication/FirstActivity.java
@@ -0,0 +1,13 @@
+package app0.com.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_activity);
+    }
+}
