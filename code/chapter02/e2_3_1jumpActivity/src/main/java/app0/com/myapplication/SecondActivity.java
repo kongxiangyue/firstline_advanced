b82a5ffb14270a28e4fd018f0eea45040aa2b596
kongxiangyue <513001363@qq.com>
2018-09-06 22:54:32 +0800

    [A] 添加两个例子

---
+++ code/chapter02/e2_3_1jumpActivity/src/main/java/app0/com/myapplication/SecondActivity.java
@@ -0,0 +1,18 @@
+package app0.com.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Kong on 2018/9/6 0006.
+ */
+
+public class SecondActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
---
+++ code/chapter02/e2_3_2jumpActivity/src/main/java/app0/com/myapplication/FirstActivity.java
@@ -0,0 +1,26 @@
+package app0.com.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_activity);
+        Button button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this
+                        , SecondActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
