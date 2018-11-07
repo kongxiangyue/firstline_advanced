5c44e8a7140cd540ad466d14e980cc7dc4b10f4d
kongxiangyue <513001363@qq.com>
2018-11-07 15:40:10 +0800

    [M] post的方式请求登录

--- added/chapter09/logintest/src/main/java/com/example/logintest/MainActivity.java
+++ added/chapter09/logintest/src/main/java/com/example/logintest/MainActivity.java
@@ -13,8 +13,10 @@ import com.google.gson.reflect.TypeToken;
 
 import java.util.List;
 
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
@@ -42,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                 username = usernameEd.getText().toString();
                 password = passwordEd.getText().toString();
 
-                loginGet();
+                loginPost();
             }
         });
     }
@@ -71,8 +73,33 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }).start();
+    }
 
 
+    private void loginPost() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+
+                    RequestBody requestBody = new FormBody.Builder()
+                            .add("username", username)
+                            .add("password",password)
+                            .build();
+
+                    Request request = new Request.Builder()
+                            .url(url)
+                            .post(requestBody)
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    showResponse(parseJSONWithGSON(responseData));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
 
