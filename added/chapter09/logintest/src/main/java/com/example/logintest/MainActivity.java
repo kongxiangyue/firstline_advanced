290493e33db2af137c89be1f52fbec9f7d9d71cb
kongxiangyue <513001363@qq.com>
2018-11-07 15:12:48 +0800

    [M] UI

--- added/chapter09/logintest/src/main/java/com/example/logintest/MainActivity.java
+++ added/chapter09/logintest/src/main/java/com/example/logintest/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.logintest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginBtn;
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindUI();
+
+        url = new String("http://172.16.33.191:5000/login");
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
     }
+
+    private void bindUI() {
+        username = (EditText) findViewById(R.id.editText);
+        password = (EditText) findViewById(R.id.editText2);
+        loginBtn = (Button) findViewById(R.id.button);
+    }
+
+    private void login() {
+
+    }
+
+
+
+
+
 }
