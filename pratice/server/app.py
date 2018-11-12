80d01b1875d19b308f70d82ab621348d544a61d4
kongxiangyue <513001363@qq.com>
2018-11-12 11:19:27 +0800

    [M] 初步接入sqlalchemy与sqlite

--- pratice/server/app.py
+++ pratice/server/app.py
@@ -1,4 +1,25 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy import Table,Column,Integer,String,MetaData,ForeignKey
+import os
+
+engine = create_engine('sqlite:///msg_board.db', echo=True)
+metadata = MetaData(engine)
+
+
+user_table = Table('user', metadata,
+        Column('id', Integer, primary_key=True),
+        Column('name', String(50)),
+        Column('fullname', String(100))
+        )
+
+address_table = Table('address', metadata,
+        Column('id', Integer, primary_key=True),
+        Column('user_id', None, ForeignKey('user.id')),
+        Column('email', String(128), nullable=False)
+        )
+
+metadata.create_all()
 
 app = Flask(__name__)
 
