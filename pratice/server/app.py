70f60942338afcfeae494b83e4ec0844e31149d1
kongxiangyue <513001363@qq.com>
2018-11-13 21:09:05 +0800

    [M] 实现向数据表插入数据 留言表，较完善

--- pratice/server/app.py
+++ pratice/server/app.py
@@ -1,9 +1,9 @@
 #coding utf-8
 
-from flask import Flask
+from flask import Flask,request
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Table,Column,Integer,String,MetaData,ForeignKey,Text,DateTime
+from sqlalchemy import Table,Column,Integer,String,MetaData,ForeignKey,Text,DateTime,desc
 from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime
 import json
@@ -16,8 +16,8 @@ Base = declarative_base()
 
 class Message(Base):
     __tablename__ = 'messages'
-    id   = Column(Integer, primary_key=True)
-    title = Column(Text(""), nullable=False)
+    id     = Column(Integer, primary_key=True)
+    title  = Column(Text(""), nullable=False)
     detail = Column(Text(""), nullable=False)
     author = Column(String(64), nullable=False)
     time   = Column(DateTime(), nullable=False)
@@ -32,19 +32,18 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-#msg = Message()
-#msg.id = 1
-#msg.time = datetime.now()
-#msg.author = 'aa'
-#msg.title = 'title'
-#msg.detail = 'detail.....'
-#
-#session.add(user)
-#session.add(msg)
+'''
+msg = Message()
+msg.id = 1
+msg.time = datetime.now()
+msg.author = 'aa'
+msg.title = 'title'
+msg.detail = 'detail.....'
+session.add(msg)
 session.commit()
+'''
 
 app = Flask(__name__)
-
 
 
 @app.before_first_request
@@ -59,7 +58,7 @@ def hello_world():
 
 @app.route('/get_all_msg')
 def get_all_msg():
-    rows = session.query(Message).all()
+    rows = session.query(Message).order_by(desc(Message.id)).all()
 
     json_obj = []
     for tmp in rows:
@@ -73,6 +72,32 @@ def get_all_msg():
 
     return json.dumps(json_obj)
     pass
+
+@app.route('/add_msg', methods=['GET', 'POST'])
+def add_msg():
+    detail = request.args.get('detail')
+    title  = request.args.get('title')
+    author = request.args.get('author')
+    if 'POST' == request.method:
+        detail = request.form['detail']
+        title  = request.form['title']
+        author = request.form['author']
+
+
+    msg        = Message()
+    msg.id     = 1 + session.query(Message).order_by(desc(Message.id)).all()[0].id
+    msg.time   = datetime.now()
+    msg.author = author
+    msg.title  = title
+    msg.detail = detail
+
+    session.add(msg)
+    session.commit()
+
+    return 'success'
+    pass
+
+
 
 
 if __name__ == '__main__':
