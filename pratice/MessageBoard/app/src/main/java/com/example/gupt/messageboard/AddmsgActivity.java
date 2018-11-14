dc39ca8909d3b2388394dacba9848a93820e8aac
kongxiangyue <513001363@qq.com>
2018-11-14 11:06:06 +0800

    [A] 新的界面

---
+++ pratice/MessageBoard/app/src/main/java/com/example/gupt/messageboard/AddmsgActivity.java
@@ -0,0 +1,16 @@
+package com.example.gupt.messageboard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Kong on 2018/11/14 0014.
+ */
+
+public class AddmsgActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
+    }
+}
