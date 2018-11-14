cd825d6389b94ee98e6d7d49b98664e797de3185
kongxiangyue <513001363@qq.com>
2018-11-14 11:40:59 +0800

    [M] 点击响应

--- pratice/MessageBoard/app/src/main/java/com/example/gupt/messageboard/MainActivity.java
+++ pratice/MessageBoard/app/src/main/java/com/example/gupt/messageboard/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.gupt.messageboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btn_add_msg : break;
+            case R.id.btn_refresh : break;
+            case R.id.btn_other : break;
+            default: break;
+        }
     }
 }
