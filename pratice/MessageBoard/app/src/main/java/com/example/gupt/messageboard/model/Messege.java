e538bc433ba85abd8549d6c6d4dc6e5975ff642c
kongxiangyue <513001363@qq.com>
2018-11-14 11:41:44 +0800

    [A] model

---
+++ pratice/MessageBoard/app/src/main/java/com/example/gupt/messageboard/model/Messege.java
@@ -0,0 +1,53 @@
+package com.example.gupt.messageboard.model;
+
+/**
+ * Created by Kong on 2018/11/14 0014.
+ */
+
+public class Messege {
+    private int id;
+    private String title;
+    private String author;
+    private String detail;
+    private String time;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
