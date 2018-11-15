9b17a9a939d6d764f8ddcc6d6dc0bbb326210fb4
kongxiangyue <513001363@qq.com>
2018-11-15 15:40:04 +0800

    [M] get请求提交添加留言，支持中文

--- pratice/MessageBoard/app/src/main/java/com/example/gupt/messageboard/AddmsgActivity.java
+++ pratice/MessageBoard/app/src/main/java/com/example/gupt/messageboard/AddmsgActivity.java
@@ -3,24 +3,133 @@ package com.example.gupt.messageboard;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.gupt.messageboard.model.Messege;
+import com.example.gupt.messageboard.util.Util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by Kong on 2018/11/14 0014.
  */
 
 public class AddmsgActivity extends Activity {
+    private EditText edit_detail;
+    private EditText edit_author;
+    private EditText edit_title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addmsg_activity);
+        bindUI();
+    }
+
+    private void bindUI() {
+        edit_detail = (EditText) findViewById(R.id.edit_detail);
+        edit_author = (EditText) findViewById(R.id.edit_author);
+        edit_title = (EditText) findViewById(R.id.edit_title);
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn_submit_get : break;
+            case R.id.btn_submit_get : handleSumitGet(); break;
             case R.id.btn_submit_post : break;
             case R.id.btn_back : finish(); break;
             default: break;
         }
     }
+
+    private boolean getUIParamASMessege(Messege msg) {
+        do {
+            msg.setAuthor(edit_author.getText().toString());
+            msg.setTitle(edit_title.getText().toString());
+            msg.setDetail(edit_detail.getText().toString());
+
+            if ("".equals(msg.getAuthor())
+                    || "".equals(msg.getTitle())
+                    || "".equals(msg.getDetail())) {
+                break;
+            }
+
+            return true;
+        } while (false);
+
+         return false;
+    }
+
+    private void handleSumitGet() {
+        Messege msg = new Messege();
+        do {
+            if (!getUIParamASMessege(msg)) {
+                Toast.makeText(this, "不能为空", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            try {
+                Request request = getGetRequest(msg);
+                doRuquest(request);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+
+
+        } while (false);
+
+    }
+
+    private Request getGetRequest(Messege msg) throws UnsupportedEncodingException {
+        String url = Util.http + Util.server_ip + Util.add_msg
+                + "?author=" + URLEncoder.encode(msg.getAuthor(), "UTF-8")
+                + "&title=" + URLEncoder.encode(msg.getTitle(), "UTF-8")
+                + "&detail=" + URLEncoder.encode(msg.getDetail(), "UTF-8");
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        return request;
+    }
+
+    private void doRuquest(final Request request) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    Response response = client.newCall(request).execute();
+                    handleSumitResult(response.body().string());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+    private void handleSumitResult(final String result) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if ("success".equals(result)) {
+                    Toast.makeText(AddmsgActivity.this
+                            , "提交成功", Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    Toast.makeText(AddmsgActivity.this
+                            , "提交失败", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+    }
+
 }
--- pratice/MessageBoard/app/src/main/java/com/example/gupt/messageboard/util/Util.java
+++ pratice/MessageBoard/app/src/main/java/com/example/gupt/messageboard/util/Util.java
@@ -5,8 +5,8 @@ package com.example.gupt.messageboard.util;
  */
 
 public class Util {
-    static public String server_ip = "172.16.33.191:5000";
-    static public String http = "http://";
-    static public String get_all_msg = "/get_all_msg";
-    static public String add_msg = "/add_msg";
+    static public final String server_ip = "172.16.33.191:5000";
+    static public final String http = "http://";
+    static public final String get_all_msg = "/get_all_msg";
+    static public final String add_msg = "/add_msg";
 }
