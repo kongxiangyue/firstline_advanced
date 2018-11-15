7ccb3fb2635f7bc1c58f233faf3f19e675c1ed58
kongxiangyue <513001363@qq.com>
2018-11-15 11:33:11 +0800

    [M] 点击响应

--- pratice/MessageBoard/app/src/main/java/com/example/gupt/messageboard/AddmsgActivity.java
+++ pratice/MessageBoard/app/src/main/java/com/example/gupt/messageboard/AddmsgActivity.java
@@ -2,6 +2,7 @@ package com.example.gupt.messageboard;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Kong on 2018/11/14 0014.
@@ -11,6 +12,15 @@ public class AddmsgActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.detail_activity);
+        setContentView(R.layout.addmsg_activity);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_submit_get : break;
+            case R.id.btn_submit_post : break;
+            case R.id.btn_back : finish(); break;
+            default: break;
+        }
     }
 }
